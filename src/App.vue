f21de8259f85719c0c983452fd27c0b4037eb103
JeffersonMontoya <112036905+JeffersonMontoya@users.noreply.github.com>
2025-12-08 14:35:09 -0500

    paginación con emit

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,54 @@
 <script setup>
 import BlogPost from "./components/BlogPost.vue";
 import { ref } from "vue";
+import PaginatePost from "./components/PaginatePost.vue";
 
 const posts = ref([]);
 
 const favoritoTitle = ref("");
+const postXpage = 10;
+const inicio = ref(0);
+const fin = ref(postXpage);
 
-const setFavorite = (title) => {
-  favoritoTitle.value = title
+const next = () => {
+  inicio.value = inicio.value + postXpage;
+  fin.value = fin.value + postXpage;
+};
+
+
+const preview = () => {
+  inicio.value = inicio.value - postXpage;
+  fin.value =fin.value - postXpage;
 }
 
 
+const setFavorite = (title) => {
+  favoritoTitle.value = title;
+};
+
 fetch("https://jsonplaceholder.typicode.com/posts")
-  .then(res => res.json())
-  .then(data => posts.value = data);
+  .then((res) => res.json())
+  .then((data) => (posts.value = data));
 </script>
 
 <template>
   <div class="container">
     <h1>App</h1>
     <h2>{{ favoritoTitle }}</h2>
+    <PaginatePost
+    @next="next" 
+    @preview="preview"
+    class="mb-2" 
+    />
 
     <BlogPost
-      v-for="post in posts"
+      v-for="post in posts.slice(inicio, fin)"
       :key="post.id"
       :title="post.title"
       :id="post.id"
       :body="post.body"
       @select-favorite="setFavorite"
+      class="mb-2"
     />
   </div>
 </template>
---
+++ src/components/PaginatePost.vue
@@ -0,0 +1,18 @@
+<script setup>
+const emit = defineEmits(["next", "preview"]);
+</script>
+
+<template>
+  <div class="btn-group" role="group" aria-label="Basic example">
+    <button
+      @click="emit('preview')"
+      type="button"
+      class="btn btn-outline-primary"
+    >
+      Preview
+    </button>
+    <button @click="emit('next')" type="button" class="btn btn-outline-primary">
+      Next
+    </button>
+  </div>
+</template>
